77dc0debe51800c4d989457003a985533237b9bc
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-04-21 15:20:26 +0400

    finished

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,19 @@ const openCardImg = document.querySelectorAll(".card .show");
 const closeCardImg = document.querySelectorAll(".card .hide");
 const star = document.querySelector(".star img");
 const cards = document.querySelectorAll(".card");
+const mrgn = screen.height/3.5;
+
+
+function scrollToJustAbove(element, margin=mrgn) {
+    let dims = element.getBoundingClientRect();
+    let ToTop = window.pageYOffset + element.getBoundingClientRect().top;
+    window.scrollTo(window.scrollX, ToTop - margin);
+}
+
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
 openCardImg.forEach((img) => {
 	img &&
 		img.addEventListener("click", (e) => {
@@ -20,8 +33,9 @@ closeCardImg.forEach((img) => {
 star &&
 	star.addEventListener("click", (e) => {
 		const random = Math.floor(Math.random() * cards.length);
+		const cardrand = cards[random];
 
 		cards.forEach((card) => card.classList.remove("active"));
-		cards[random] &&
-			(cards[random].classList.add("active"), cards[random].scrollIntoView());
+		cardrand &&
+			(cardrand.classList.add("active"), scrollToJustAbove(cardrand));
 	});
