16833f9a27bee5a984e9f2bb864ad21b51ef55b5
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-04-21 15:45:15 +0400

    mobile fix

---
+++ index.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link
+			rel="preload"
+			href="/src/font/FiraGO-Italic.otf"
+			as="font"
+			type="font/otf"
+			crossorigin
+		/>
+		<link
+			rel="preload"
+			href="/src/font/FiraGO-HeavyItalic.otf"
+			as="font"
+			type="font/otf"
+			crossorigin
+		/>
+		<link
+			rel="preload"
+			href="/src/font/FiraGO-Regular.ttf"
+			as="font"
+			type="font/ttf"
+			crossorigin
+		/>
+
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>მთავარი - სვლა</title>
+		<link rel="stylesheet" href="src/css/style.css?v=1.9" />
+		<link
+			id="icon"
+			rel="shortcut icon"
+			href="src/img/magic-star.svg"
+			type="image/x-icon"
+		/>
+		<script
+			async
+			src="https://www.googletagmanager.com/gtag/js?id=G-75D547BXDQ"
+		></script>
+		
+		
+        <meta property="og:url" content="https://svla.ge">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="სვლა">
+        <meta property="og:description" content="პირველად იყო ბგერა და არ ვიცით, ყველაფრისთვის თავის სახელის დარქმევის სურვილმა ქნა თუ ჩვენმა დაუდგომელმა ცნობისმოყვარეობამ, მაგრამ, ფაქტია, ასო-ბგერები ძვირფას კონებად შევკარით და სიტყვები გამოვიგონეთ.">
+        <meta property="og:image:alt" content="სვლა">
+        <meta property="fb:app_id" content="" />
+        <meta property="og:image" content="https://svla.ge/src/img/svlabg.png">
+        <meta property="og:site_name" content="სვლა">
+        <meta property="og:locale" content="ka_GE">
+  
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="header-wrapper">
+					<a class="logo" href="#">
+						<img src="src/img/logo.svg" alt="" />
+					</a>
+					<div class="star">
+						<img src="src/img/magic-star.svg" alt="" />
+					</div>
+				</div>
+			</div>
+		</header>
+		<main>
+			<secion class="main-page">
+				<div class="container">
+					<div class="main-page-wrapper">
+						<h2 class="title" text="მოუბარი">მოუბარი</h2>
+						<p class="main-page-desc">
+							პირველად იყო ბგერა და არ ვიცით, ყველაფრისთვის თავის სახელის
+							დარქმევის სურვილმა ქნა თუ ჩვენმა დაუდგომელმა ცნობისმოყვარეობამ,
+							მაგრამ, ფაქტია, ასო-ბგერები ძვირფას კონებად შევკარით და სიტყვები
+							გამოვიგონეთ. <br/> ამის შემდეგ, ყვავილების სადარი ანბანითა და
+							სიტყვაკაზმულობით, ფონსაც, მეტ-ნაკლებად, იოლად გავდივართ და იმის
+							მიუხედავად, რომ განმარტება ყველაფერს მოეძებნება, ზოგჯერ, სიტყვები
+							მაინც არ გვყოფნის.
+						</p>
+
+						<div class="cards-wrapper">
+							<div class="card">
+								<img class="card-img" src="src/img/khedva.png" alt="ხედვა" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ხედვა</h4>
+									<p>
+										გონებაში აღბეჭდილი მკაფიო სურათი ან წარმოდგენა, რომელიც შთაგაგონებს, განსაზღვრავს მიმართულებას და გეხმარება სასურველი მიზნისკენ სწრაფვაში.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img" src="src/img/survili.png" alt="სურვილი" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>სურვილი</h4>
+									<p>
+										გულიდან წამოსული შეგრძნება თუ ვნება, რომელიც  აძლიერებს მოტივაციას და გვაიძულებს წინსვლას; გვიბიძგებს მოქმედებისკენ და გვეხმარება სანუკვარ ოცნებებთან მიახლოებაში.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img" src="src/img/moqmedeba.png" alt="მოქმედება" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>მოქმედება</h4>
+									<p>
+										მიზანმიმართული ძალისხმევა, რომელიც აქცევს ოცნებებს რეალობად; გამბედაობითა და მონდომებით გადადგმული ნაბიჯები, წარმატების უმოკლეს დროში მისაღწევად.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img" src="src/img/mizani.png" alt="მიზანი" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>მიზანი</h4>
+									<p>
+										განზრახვა, რომელიც შთააგონებს მოქმედებას, აყალიბებს
+										მიმართულებას და აძლიერებს მოტივაციას სასურველ წერტილთან
+										მისასვლელად.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<!-- <div class="card">
+								<img class="card-img" src="src/img/Love.png" alt="სიყვარული" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>სიყვარული</h4>
+									<input
+										type="text"
+										class="write-love"
+										placeholder="ჩაწერე შენი განმარტება"
+									/>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div> -->
+							<div class="card">
+								<img class="card-img" src="src/img/energia.png" alt="ენერგია" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ენერგია</h4>
+									<p>
+										ძალა, რომელიც უბიძგებს ცოცხალ ორგანიზმებს მოქმედებისკენ; სიცოცხლისუნარიანობის მომნიჭებელი მუხტი, რომელიც ზრდასა და ცვლილებებს უზრუნველყოფს. 
+									</p>
+
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img" src="src/img/mukhti.png" alt="მუხტი" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>მუხტი</h4>
+									<p>
+										ენერგიისა თუ შთაგონების უეცარი, ელექტრული აფეთქება, რომელიც
+										ბიძგს აძლევს მოქმედებას, აძლიერებს მას და იწვევს პოზიტიურ
+										ცვლილებებსა და ტრანსფორმაციას.
+									</p>
+
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+
+							<div class="card">
+								<img
+									class="card-img"
+									src="src/img/shtagoneba.png"
+									alt="შთაგონება"
+								/>
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>შთაგონება</h4>
+									<p>
+										შემოქმედებითად განწყობა, აზრის ჩანერგვა, მიგნება; ძალა, რომელიც მუხტავს შემოქმედებასა თუ მოქმედებას.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+
+							<div class="card">
+								<img class="card-img" src="src/img/droa.png" alt="დრო" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>დრო</h4>
+									<p>
+										ცნება, რომელიც რეფრენად გასდევს და განსაზღვრავს ჩვენს ცხოვრებას; უნივერსალური რესურსი, რომელიც მუდმივად გვიბოძებს განვითარების შესაძლებლობებსა და ძვირფას მოგონებებს.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<!-- <div class="card">
+								<img class="card-img" src="src/img/cvlileba.jpg" alt="ცვლილება" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ცვლილება</h4>
+									<p>
+										ზრდის, ევოლუციისა ან ადაპტაციის გარდაუვალი პროცესი; გარდამტეხი ქმედება, რომელიც ცხოვრების ახალ გეზს განსაზღვრავს.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div> -->
+
+							<div class="card">
+								<img class="card-img" src="src/img/gabedva.png" alt="გაბედვა" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>გაბედვა</h4>
+									<p>
+										თამამად წასვლა რისკსა თუ გამოწვევაზე; საკუთარ შესაძლებლობებზე გამარჯვება შიშზე ამაღლებით.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img
+									class="card-img"
+									src="src/img/motivacia.png"
+									alt="მოტივაცია"
+								/>
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>მოტივაცია</h4>
+									<p>
+										ენთუზიაზმი, რომელიც მოქმედებისკენ გვიბიძგებს; სწრაფვა, რომელიც აძლიერებს ცვლილებებს და ხელს უწყობს წარმატებას.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+
+							<div class="card">
+								<img class="card-img" src="src/img/shekmna.png" alt="შექმნა" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>შექმნა</h4>
+									<p>
+										ხედვით, ფანტაზიითა და მხატვრული მოქმედებით განმსჭვალული,
+										ჯერაც არარსებულისა და უნიკალურის არსებობაში შემოტანის აქტი.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img
+									class="card-img"
+									src="src/img/gardatekha.png"
+									alt="გარდატეხა"
+								/>
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>გარდატეხა</h4>
+									<p>
+										მიმართულების შეცვლის გადამწყვეტი მოვლენა; ერთი მდგომარეობიდან, სიტუაციიდან და პერსპექტივიდან მეორეზე გადასვლის მკაფიო პროცესი, რომელსაც თან ახლავს მნიშვნელოვანი ცვლილებები თუ სრული მეტამორფოზა.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img
+									class="card-img"
+									src="src/img/sibrdzne.png"
+									alt="სიბრძნე"
+								/>
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>სიბრძნე</h4>
+									<p>
+										სიღრმისეული განსჯა გამჭრიახი გონებით, რომელიც ხელმძღვანელობს მრავალფეროვანი ცოდნითა და ყურადსაღები გამოცდილებით.
+									</p>
+
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img
+									class="card-img"
+									src="src/img/ganwkoba.png"
+									alt="განწყობა"
+								/>
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>განწყობა</h4>
+									<p>
+										ემოციური ატმოსფერო ან აურა, რომლითაც გაჟღენთილია ადამიანი, გარემო თუ ვითარება; გამოხატულია მკვეთრი განცდების ერთობლიობით, რომელიც გავლენას ახდენს აღქმებსა და ურთიერთქმედებებზე.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img" src="src/img/vneba.png" alt="ვნება" />
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ვნება</h4>
+									<p>
+										სულის სიღრმიდან დაძრული მძაფრი ემოცია, რომელიც მუხტავს ენთუზიაზმს, მიზანსა თუ მოქმედებას.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img" src="src/img/dzala.png" alt="ძალა" />
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ძალა</h4>
+									<p>
+										შინაგანი ენერგიის, ცოდნისა და რწმენის ერთობლიობა, რომელიც გაძლევს შესაძლებლობას, გადადგა მყარი და მკაფიო ნაბიჯები.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+							<!-- <div class="card">
+								<img
+									class="card-img"
+									src="src/img/cnobismokvareoba.png"
+									alt="ცნობისმოყვარეობა"
+								/>
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ცნობისმოყვარეობა</h4>
+									<p>
+										თანდაყოლილი ან შეძენილი სურვილი, შეისწავლო, გაიგო და შეიმეცნო აქამდე უცნობი; ვნება, რომელიც ახალი ცოდნისა და აღმოჩენებისა სამყაროსკენ  მიგვაქანებს.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div> -->
+							<div class="card">
+								<img
+									class="card-img"
+									src="src/img/azrovneba.png"
+									alt="აზროვნება"
+								/>
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>აზროვნება</h4>
+									<p>
+										მსჯელობის, ანალიზისა და შეფასების თვითკმარი პროცესი, რომელიც სამყაროს უნიკალურ აღქმას განაპირობებს.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+
+							<div class="card">
+								<img class="card-img" src="src/img/nakadi.png" alt="ნაკადი" />
+
+								<img src="src/img/Vector.svg" alt="" class="show" />
+								<div class="desc">
+									<h4>ნაკადი</h4>
+									<p>
+										ერთიანი ძალა, რომელიც იზრდება პროგრესულად და ძლიერი ენერგიით მიიწევს წინ; აღმაფრთოვანებელი ტალღა, რომელიც მნიშვნელოვანი ცვლილებების  საწყისი ხდება.
+									</p>
+									<img src="src/img/Vector.svg" class="hide" alt="" />
+								</div>
+							</div>
+						</div>
+
+						<a href="/about" class="see-more">პროექტის შესახებ</a>
+					</div>
+				</div>
+			</secion>
+			<div class="shadows">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<footer>
+				<div class="container">
+					<p>
+						ხელქმნა <a href="https://smartacademy.ge/">სმარტ აკადემიამ</a> და
+						<a href="https://smartweb.ge/">სმარტ ვებმა</a>
+					</p>
+				</div>
+			</footer>
+		</main>
+
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag("js", new Date());
+			gtag("config", "G-75D547BXDQ");
+		</script>
+
+		<script src="src/js/main.js?v=1.8"></script>
+	</body>
+</html>
--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,17 @@ const openCardImg = document.querySelectorAll(".card .show");
 const closeCardImg = document.querySelectorAll(".card .hide");
 const star = document.querySelector(".star img");
 const cards = document.querySelectorAll(".card");
-const mrgn = screen.height/3.5;
+const cardImgs = document.querySelectorAll(".card-img");
+const mrgn = screen.height / 3.5;
 
-
-function scrollToJustAbove(element, margin=mrgn) {
-    let dims = element.getBoundingClientRect();
-    let ToTop = window.pageYOffset + element.getBoundingClientRect().top;
-    window.scrollTo(window.scrollX, ToTop - margin);
+function scrollToJustAbove(element, margin = mrgn) {
+	let dims = element.getBoundingClientRect();
+	let ToTop = window.pageYOffset + element.getBoundingClientRect().top;
+	window.scrollTo(window.scrollX, ToTop - margin);
 }
 
-if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';
+if ("scrollRestoration" in history) {
+	history.scrollRestoration = "manual";
 }
 
 openCardImg.forEach((img) => {
@@ -30,12 +30,28 @@ closeCardImg.forEach((img) => {
 		});
 });
 
+cards.forEach((card) => {
+	if (card && window.innerWidth < 1024) {
+		card.addEventListener("click", (e) => {
+			cards.forEach((card) => card.classList.remove("active"));
+			if (
+				e.target.classList.contains("card") ||
+				e.target.classList.contains("show")
+			) {
+				card.classList.add("active");
+			}
+			if (e.target.classList.contains("desc")) {
+				card.classList.remove("active");
+			}
+		});
+	}
+});
+
 star &&
 	star.addEventListener("click", (e) => {
 		const random = Math.floor(Math.random() * cards.length);
 		const cardrand = cards[random];
 
 		cards.forEach((card) => card.classList.remove("active"));
-		cardrand &&
-			(cardrand.classList.add("active"), scrollToJustAbove(cardrand));
+		cardrand && (cardrand.classList.add("active"), scrollToJustAbove(cardrand));
 	});
